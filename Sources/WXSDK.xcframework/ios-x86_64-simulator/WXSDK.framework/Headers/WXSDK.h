446239d41ce14973c63aec9e4eee82c8a5ba2d27
泽i <zhangzh@eagersoft.cn>
2021-11-08 13:54:54 +0800

    更新

---
+++ Sources/WXSDK.xcframework/ios-x86_64-simulator/WXSDK.framework/Headers/WXSDK.h
@@ -0,0 +1,21 @@
+//
+//  WXSDK.h
+//  WXSDK
+//
+//  Created by youzy01 on 2021/11/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WXSDK.
+FOUNDATION_EXPORT double WXSDKVersionNumber;
+
+//! Project version string for WXSDK.
+FOUNDATION_EXPORT const unsigned char WXSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WXSDK/PublicHeader.h>
+
+
+#import <WXSDK/WXApi.h>
+#import <QQSDK/WXApiObject.h>
+#import <QQSDK/WechatAuthSDK.h>
